849db630426ab659671a0b7cca20150519f9f93b
CesJauregui <cjaureguisaavedra@gmail.com>
2024-03-29 17:41:09 -0500

    Se deshabilita el api sanctum y el supports_credentials

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,10 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('auth/register', [AuthController::class, 'register']);
-Route::post('auth/login', [AuthController::class, 'login']);
-
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('users', [AuthController::class, 'index']);
-    Route::get('auth/logout', [AuthController::class, 'logout']);
+    Route::delete('logout', [AuthController::class, 'logout']);
 });
