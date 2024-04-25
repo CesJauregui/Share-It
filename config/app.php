528ebb0ca320ef0b1de83a9121bf10f4e4da75ae
CesJauregui <cjaureguisaavedra@gmail.com>
2024-04-24 19:28:39 -0500

    Se agrega Filament para el panel administrativo

--- config/app.php
+++ config/app.php
@@ -109,7 +109,7 @@ return [
     |
     */
 
-    'faker_locale' => 'en_US',
+    'faker_locale' => 'es_PE',
 
     /*
     |--------------------------------------------------------------------------
@@ -167,6 +167,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
+        App\Providers\Filament\AdminPanelProvider::class,
         App\Providers\RouteServiceProvider::class,
     ])->toArray(),
 
